ead4006eaffdff5ec26a77bd4478b52e8784b37b
Malik Robinson <maliklorenzorobinson@gmail.com>
2017-10-13 12:29:24 -0400

    header fixed

--- inc/connection.php
+++ inc/connection.php
@@ -1,6 +1,7 @@
 <?php
+
 // connect database with webpage
-$db = new PDO("mysql:host=localhost;dbname=mrobinson_challengeone", "r2hstudent", "SbFaGzNgGIE8kfP");
+$db = new PDO("mysql:host=localhost;dbname=mrobinson_challengeone;port:3306", "r2hstudent", "SbFaGzNgGIE8kfP");
 $db ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 // create an associative array from the products table with each row as a value
 $products = $db->query("SELECT * FROM products");
